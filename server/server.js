c86b7856920f65707ebf32aa83b199af7504e799
Jason McDermott <jmcdermott@rightpoint.com>
2020-05-04 19:51:20 -0400

    add placeholder

--- server/server.js
+++ server/server.js
@@ -10,4 +10,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.listen(port, () => console.log(`server is running on port${port}`))
+app.listen(port, () => console.log(`server is running on port ${port}`))
--- src/components/PictureOfTheDay.js
+++ src/components/PictureOfTheDay.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-const NASA_APOD_API_KEY = process.env.NASA_APOD_API_KEY || require('../../config').NASA_APOD_API_KEY
+const NASA_APOD_API_KEY = process.env.NASA_APOD_API_KEY //|| require('../../config').NASA_APOD_API_KEY
 
 export default class PictureOfTheDay extends React.Component {
 
@@ -35,13 +35,31 @@ export default class PictureOfTheDay extends React.Component {
     }
     
     render() {
-        const { error, picture } = this.state
+        const { error, isLoaded, picture } = this.state
         if (error) {
             return <div>Error: {error.message}</div>
-        } else {
+        } else if (!isLoaded) {
             return (
                 <div className="potd-container">
                     <img src={picture.url} className="picture" alt="picture of the day" />
+                    <div className="top-left boxed">
+                        Pic of the Day
+                    </div>
+                    <div className="bottom-right boxed">
+                        <div className="title">{picture.title}</div>
+                        <div className="description module overflow">
+                            <p>{picture.explanation}</p>
+                        </div>
+                        <div className="read-more">
+                            <NavLink to="/pic-of-the-day" activeClassName="is-active">(read more)</NavLink>
+                        </div> 
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="potd-container">
+                    <img src="../../../placeholder.jpg" className="picture" alt="picture of the day" />
                     <div className="top-left boxed">
                         Pic of the Day
                     </div>
